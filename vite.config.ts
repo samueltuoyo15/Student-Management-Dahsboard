232990179133a3d68ef58b129505783525aa91eb
samueltuoyo15 <samueltuoyo9082@gmail.com>
2024-10-30 19:09:11 +0100

    made my project full .ts extension

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
+import tailwindcss from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
 export default defineConfig({
   plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [
+        tailwindcss(), 
+        autoprefixer(), 
+      ],
+    },
+  }
 })
